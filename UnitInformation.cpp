53de3f011c9a63ba4634f3e4a0bed5dcedd6045a
Jayanta Nath <jayantanth@gmail.com>
2015-12-01 21:15:02 +0530

    Create UnitInformation.cpp

---
+++ UnitInformation.cpp
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////////////////
+// File:        UnitInformation.cpp
+// Description: Container of segmented units information.
+// Author:      Md. Abul Hasnat
+// Organization Center for Research on Bangla Language Processing (CRBLP)
+// Version:		0.7
+///////////////////////////////////////////////////////////////////////
+
+
+#include "StdAfx.h"
+#include "UnitInformation.h"
+
+UnitInformation::UnitInformation(void)
+{
+}
